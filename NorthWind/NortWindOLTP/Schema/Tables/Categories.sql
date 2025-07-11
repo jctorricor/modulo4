d34219d1f8955af3dc58b6c3f687abe325be392e
Rene Pacheco <pacheco.rene@gmail.com>
2025-07-11 14:58:06 -0400

    Categories table test column create and delete

--- NorthWind/NortWindOLTP/Schema/Tables/Categories.sql
+++ NorthWind/NortWindOLTP/Schema/Tables/Categories.sql
@@ -3,7 +3,7 @@
 	[CategoryName] [nvarchar](15) NOT NULL,
 	[Description] [nvarchar](max) NULL,
 	[Picture] [image] NULL,
-	[rowversion] [timestamp] NULL,
+	[rowversion] [timestamp] NULL,	
  CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
 (
 	[CategoryID] ASC
