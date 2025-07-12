00c77c1c2d881e134d961c89a0e6ace01c77709d
Juan Carlos Torrico Rios <juan.torrico.rios@gmail.com>
2025-07-11 23:09:51 -0400

    add RegionCode

--- NorthWind/NortWindOLTP/Schema/Tables/Region.sql
+++ NorthWind/NortWindOLTP/Schema/Tables/Region.sql
@@ -2,6 +2,7 @@
 	[RegionID] [int] NOT NULL,
 	[RegionDescription] [nchar](50) NOT NULL,
 	[rowversion] [timestamp] NULL,
+	[RegionCode]  [nchar](50),
  CONSTRAINT [PK_Region] PRIMARY KEY NONCLUSTERED 
 (
 	[RegionID] ASC))
