f879a96567386d19777b5150db81fe307fa75a87
Rene Pacheco <pacheco.rene@gmail.com>
2025-07-19 19:17:46 -0400

    add partial columns to DimCustomers

--- NorthWindDW/Schema/Tables/DimCustomers.sql
+++ NorthWindDW/Schema/Tables/DimCustomers.sql
@@ -1,5 +1,17 @@
 ﻿CREATE TABLE [dbo].[DimCustomers]
 (
-    col1 int NOT NULL
+	-- tabla Customers
+	[CustomerSK] [int] IDENTITY(1,1) NOT NULL CONSTRAINT PK_DimCustomer PRIMARY KEY,
+    [CustomerID] [nchar](5) NOT NULL,
+	[CompanyName] [nvarchar](40) NOT NULL,
+	[ContactName] [nvarchar](30) NULL,
+	[ContactTitle] [nvarchar](30) NULL,
+	[Address] [nvarchar](60) NULL,
+	[City] [nvarchar](15) NULL,
+	[Region] [nvarchar](15) NULL,
+	[PostalCode] [nvarchar](10) NULL,
+	[Country] [nvarchar](15) NULL,
+	[Phone] [nvarchar](24) NULL,
+	[Fax] [nvarchar](24) NULL,
 )
 GO
