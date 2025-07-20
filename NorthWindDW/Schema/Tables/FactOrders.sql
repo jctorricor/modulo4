9a8e415f9118eca442aa8da881fadac658463246
Juan Carlos Torrico Rios <juan.torrico.rios@gmail.com>
2025-07-19 20:30:01 -0400

    Add FactOrders

--- NorthWindDW/Schema/Tables/FactOrders.sql
+++ NorthWindDW/Schema/Tables/FactOrders.sql
@@ -1,15 +1,17 @@
 ﻿CREATE TABLE [dbo].[FactOrders](
-	[OrderID] [int] IDENTITY(1,1) NOT NULL,
+	[OrderID] [int]  NOT NULL,
+	[ProductID] [int]  NOT NULL,
 	[CustomerSK] [int] NOT NULL CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerSK]) REFERENCES [dbo].[DimCustomers] ([CustomerSK]),
 	[EmployeeSK] [int] NOT NULL CONSTRAINT [FK_Orders_Employees] FOREIGN KEY([EmployeeSK]) REFERENCES [dbo].[DimEmployees] ([EmployeeSK]),
-	[OrderDateSK] [int] NOT NULL CONSTRAINT [FK_Orders_OrderDate] FOREIGN KEY([OrderDateSK]) REFERENCES [dbo].[DimDate] ([DateKey]),
-	[RequiredDateSK] [int] NOT NULL CONSTRAINT [FK_Orders_[RequiredDate] FOREIGN KEY(RequiredDateSK) REFERENCES [dbo].[DimDate] ([DateKey]),
-	[ShippedDateSK] [int] NOT NULL CONSTRAINT [FK_Orders_ShippedDate] FOREIGN KEY([ShippedDateSK]) REFERENCES [dbo].[DimDate] ([DateKey]),
+	[OrderDateKey] [int] NOT NULL CONSTRAINT [FK_Orders_OrderDate] FOREIGN KEY([OrderDateKey]) REFERENCES [dbo].[DimDate] ([DateKey]),
+	[RequiredDateKey] [int] NOT NULL CONSTRAINT [FK_Orders_[RequiredDate] FOREIGN KEY(RequiredDateKey) REFERENCES [dbo].[DimDate] ([DateKey]),
+	[ShippedDateKey] [int] NOT NULL CONSTRAINT [FK_Orders_ShippedDate] FOREIGN KEY([ShippedDateKey]) REFERENCES [dbo].[DimDate] ([DateKey]),
 	--[ShipperSK] [int] NOT NULL CONSTRAINT [FK_Orders_Shipper] FOREIGN KEY(ShipperSK) REFERENCES [dbo].[DimShippers] ([ShipperSK]),	
 	[ProductSK] [int] NOT NULL CONSTRAINT [FK_Orders_Product] FOREIGN KEY(ProductSK) REFERENCES [dbo].[DimProducts] ([ProductSK]),
 	[UnitPrice] [money] NOT NULL,
 	[Quantity] [smallint] NOT NULL,
 	[Discount] [real] NOT NULL,
-	[Freight] [money] NULL 
+	[Freight] [money] NULL,	
+	CONSTRAINT PK_FactOrders PRIMARY KEY(OrderID,ProductID)
 )
 GO
