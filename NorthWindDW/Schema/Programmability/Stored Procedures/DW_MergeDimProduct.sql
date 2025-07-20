b361d052b59459b14e325298ba1b2666442369ef
ctola91 <ctolapacheco@gmail.com>
2025-07-19 21:53:12 -0400

    Add DW_MergeDimProduct SP

---
+++ NorthWindDW/Schema/Programmability/Stored Procedures/DW_MergeDimProduct.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimProduct]
+AS
+BEGIN
+
+	UPDATE dp
+	SET 
+	    [ProductID]      = sp.[ProductID],
+        [ProductName]    = sp.[ProductName],
+        [QuantityPerUnit]= sp.[QuantityPerUnit],
+        [UnitPrice]      = sp.[UnitPrice],
+        [UnitsInStock]   = sp.[UnitsInStock],
+        [UnitsOnOrder]   = sp.[UnitsOnOrder],
+        [ReorderLevel]   = sp.[ReorderLevel],
+        [Discontinued]   = sp.[Discontinued],
+        [CategoryName]   = sp.[CategoryName],
+        [Description]    = sp.[Description],
+        [Picture]        = sp.[Picture],
+        [CompanyName]    = sp.[CompanyName],
+        [ContactName]    = sp.[ContactName],
+        [ContactTitle]   = sp.[ContactTitle],
+        [Address]        = sp.[Address],
+        [City]           = sp.[City],
+        [Region]         = sp.[Region],
+        [PostalCode]     = sp.[PostalCode],
+        [Country]        = sp.[Country],
+        [Phone]          = sp.[Phone],
+        [Fax]            = sp.[Fax],
+        [HomePage]       = sp.[HomePage]
+	FROM [dbo].[DimProduct] dp
+    INNER JOIN [staging].[Product] sp ON dp.[ProductSK] = sp.[ProductSK];
+END
+GO
