e632960ecf39203c660fd66f049e7e0185215d7e
ctola91 <ctolapacheco@gmail.com>
2025-07-24 22:31:03 -0400

    Add ETL process for product table

---
+++ NorthWind/NortWindOLTP/Schema/Programmability/Stored Procedures/GetProductChangesByRowVersion.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[GetProductChangesByRowVersion]
+	@startRow BIGINT 
+   ,@endRow  BIGINT 
+AS
+BEGIN
+	SELECT [ProductID]
+      ,[ProductName]
+      ,[SupplierID]
+      ,[CategoryID]
+      ,[QuantityPerUnit]
+      ,[UnitPrice]
+      ,[UnitsInStock]
+      ,[UnitsOnOrder]
+      ,[ReorderLevel]
+      ,[Discontinued]
+      ,[rowversion]
+	FROM [dbo].[Products]
+	WHERE [rowversion] > CONVERT(ROWVERSION,@startRow) 
+	AND [rowversion] <= CONVERT(ROWVERSION,@endRow)
+END
+GO
