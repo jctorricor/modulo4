90e24abef110acfff32ecd526522eea929df032a
Rene Pacheco <pacheco.rene@gmail.com>
2025-07-27 12:05:33 -0400

    adding dimshipper

---
+++ NorthWind/NortWindOLTP/Schema/Programmability/Stored Procedures/GetShipperChangesByRowVersion.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetShipperChangesByRowVersion]
+	@startRow BIGINT 
+   ,@endRow  BIGINT 
+AS
+BEGIN
+	SELECT [ShipperID]
+      ,[CompanyName]
+      ,[Phone]
+      ,[Address]      
+  FROM [dbo].[Shippers]
+	WHERE [rowversion] > CONVERT(ROWVERSION,@startRow) 
+	AND [rowversion] <= CONVERT(ROWVERSION,@endRow)  
+END
+GO
